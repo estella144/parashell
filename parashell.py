cc1f82902a13b09a60039f1dc3768ec46a812f44
Oliver <oliverng2512@icloud.com>
2024-03-15 18:29:01 +0000

    overall updates (0.1.3)

--- parashell.py
+++ parashell.py
@@ -1,77 +1,80 @@
-#    parashell - a shell
-#    Copyright (C) 2024 Oliver Nguyen
-#
-#    This program is free software: you can redistribute it and/or modify
-#    it under the terms of the GNU General Public License as published by
-#    the Free Software Foundation, either version 3 of the License, or
-#    (at your option) any later version.
-#
-#    This program is distributed in the hope that it will be useful,
-#    but WITHOUT ANY WARRANTY; without even the implied warranty of
-#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-#    GNU General Public License for more details.
-#
-#    You should have received a copy of the GNU General Public License
-#    along with this program.  If not, see <https://www.gnu.org/licenses/>.
+##    parashell - a shell
+##    Copyright (C) 2024 Oliver Nguyen
+##
+##    This program is free software: you can redistribute it and/or modify
+##    it under the terms of the GNU General Public License as published by
+##    the Free Software Foundation, either version 3 of the License, or
+##    (at your option) any later version.
+##
+##    This program is distributed in the hope that it will be useful,
+##    but WITHOUT ANY WARRANTY; without even the implied warranty of
+##    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+##    GNU General Public License for more details.
+##
+##    You should have received a copy of the GNU General Public License
+##    along with this program.  If not, see <https://www.gnu.org/licenses/>.
 
 import os
 import platform
 import sys
 
-SCRIPT_VERSION = "0.1.0"
-SCRIPT_DATE = "12 Mar 2024"
+VERSION = "0.1.3"
+DATE = "15 Mar 2024"
+
 NOTICE = """Parashell Copyright (C) 2024 Oliver Nguyen
 This program comes with ABSOLUTELY NO WARRANTY; for details type `show w'.
 This is free software, and you are welcome to redistribute it
 under certain conditions; type `show c' for details."""
 
-
 def info():
-    print(f"Parashell v{SCRIPT_VERSION} ({SCRIPT_DATE}) on {sys.platform}")
+    print(f"Parashell {VERSION} ({DATE}) on {sys.platform}")
     print(f"Python:   {sys.version}")
     print(f"Platform: {platform.system()} {platform.release()} ({platform.platform()})")
     if platform.system() == "Windows":
         release, version, csd, ptype = platform.win32_ver()
-        print(f"Windows:  {release} {platform.win32_edition()} ({version}) {csd} {ptype}")
+        edition = platform.win32_edition()
+        print(f"Windows:  {release} {edition} {csd} ({version}) {ptype}")
     elif platform.system() == "Darwin":
-        # macOS
         release, versioninfo, machine = platform.mac_ver()
         print(f"macOS:    {release} on {machine}")
 
-print("Starting Parashell...")
-print()
 print(NOTICE)
 print()
 info()
 print()
-print("Please ensure full paths (not relative) are used with cd.")
-print("Type 'help' for help.")
+print("For cd, please enter full (absolute) path - not relative path.")
+print("Type help for help.")
+print()
 
 while True:
-    cwd = os.getcwd()
-    cmd = input(f"{cwd}> ")
+    cd = os.getcwd()
+    cmd = input(f"{cd}> ")
     if cmd.startswith("cd"):
-        cl = cmd.split(" ", 1)
-        os.chdir(cl[1])
-        print(f"Directory successfully changed to {cl[1]}")
+        try:
+            cl = cmd.split(" ", 1)
+            os.chdir(cl[1])
+        except IndexError:
+            print("Error: 1 argument required for cd.")
+        except FileNotFoundError:
+            print("Error: Directory not found.")
+        except NotADirectoryError:
+            print("Error: Not a directory.")
     elif cmd == "help":
-        if platform.system() == "Windows":
-            print("Enter any command you would normally enter into cmd.")
-            print("Type 'exit' to exit.")
-            print("'cd' only accepts full paths.")
-        else:
-            print("Enter any command you would normally enter into Terminal.")
-            print("'cd' only accepts full paths.")
+        print("Type any command you would normally type in your console/shell.")
+        print("Type info for program info.")
     elif cmd == "show w":
-        print("See section 15 of the GNU GPL on <https://www.gnu.org/licenses/>.")
+        print("Refer to the GNU GPL, section 15 <https://www.gnu.org/licenses/>.")
     elif cmd == "show c":
-        print("See sections 4-6 of the GNU GPL on <https://www.gnu.org/licenses/>.")
+        print("Refer to the GNU GPL, section 4-6 <https://www.gnu.org/licenses/>.")
+    elif cmd == "info":
+        info()
     elif cmd == "exit":
-        print("Goodbye")
         break
     else:
         exit_code = os.system(cmd)
         if exit_code != 0:
-            print(f"{cmd}: Failure (exit code {exit_code})")
+            print(f"Fail (exit code {exit_code})")
         else:
-            print(f"{cmd}: Success (exit code {exit_code})")
+            print(f"Success (exit code {exit_code})")
+
+print("Goodbye")
