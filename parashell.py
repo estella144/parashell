d757ce2bf400736b21fc18d630faa6a16e17a802
Oliver <oliverng2512@icloud.com>
2024-03-15 20:55:45 +0000

    add preloaded paginated dir listing

--- parashell.py
+++ parashell.py
@@ -16,15 +16,19 @@
 
 import os
 import platform
+import shutil
+import subprocess
 import sys
 
-VERSION = "0.1.3"
+VERSION = "0.2.0"
 DATE = "15 Mar 2024"
 
 NOTICE = """Parashell Copyright (C) 2024 Oliver Nguyen
 This program comes with ABSOLUTELY NO WARRANTY; for details type `show w'.
 This is free software, and you are welcome to redistribute it
 under certain conditions; type `show c' for details."""
+
+page_idx = 0
 
 def info():
     print(f"Parashell {VERSION} ({DATE}) on {sys.platform}")
@@ -45,6 +49,7 @@ print()
 print("For cd, please enter full (absolute) path - not relative path.")
 print("Type help for help.")
 print()
+input("[Enter] - Continue")
 
 def clear_screen():
     if platform.system() == "Windows":
@@ -53,22 +58,88 @@ def clear_screen():
         # macOS or Linux
         os.system("clear")
 
+def get_dir_output():
+    if platform.system() == "Windows":
+        out = subprocess.check_output("dir", shell=True)
+    else:
+        # macOS or Linux
+        out = subprocess.check_output("ls -l", shell=True)
+    return out.decode("utf-8")
+
+def paginate_output(out):
+    lines = out.split("\n")
+    if platform.system() == "Windows":
+        header_lines = 5
+        footer_lines = 2
+        footer = lines[-footer_lines:]
+        content = lines[header_lines:-footer_lines]
+    else:
+        # macOS or Linux
+        header_lines = 1
+        footer_lines = 0
+        footer = []
+        content = lines[header_lines:]
+    header = lines[0:header_lines]
+    pages = [content[i:i+12] for i in range(0, len(content), 12)]
+    if len(content) % 12 != 0:
+        remaining_lines = content[len(content)//12*12:]
+        pages.append(remaining_lines)
+    return [header, footer, pages]
+
+def print_page(header, footer, pages, page_idx, cd):
+    global VERSION
+    columns, lines = shutil.get_terminal_size()
+
+    if page_idx != 0:
+        left_arrow = "< "
+    else:
+        left_arrow = ""
+
+    if page_idx != len(pages)-1:
+        right_arrow = " >"
+    else:
+        right_arrow = ""
+
+    current_page = pages[page_idx]
+    top_divider_msg = f"[Parashell {VERSION} - {cd}]"
+    bottom_divider_msg = f"[{left_arrow}Page {page_idx+1} of {len(pages)-1}{right_arrow}]"
+
+    print(f"{top_divider_msg:=^{columns}}")
+    print('\n'.join(header))
+    print('\n'.join(current_page))
+    print('\n'.join(footer))
+    print(f"{bottom_divider_msg:=^{columns}}")
+
 while True:
+    clear_screen()
     cd = os.getcwd()
+    output = get_dir_output()
+    header, footer, pages = paginate_output(output)
+    print_page(header, footer, pages, page_idx, cd)
+
     cmd = input(f"{cd}> ")
     if cmd.startswith("cd"):
         try:
             cl = cmd.split(" ", 1)
             os.chdir(cl[1])
+            print(f"Success: changed directory to {cl[1]}")
         except IndexError:
             print("Error: 1 argument required for cd.")
+            input("[Enter] - Continue")
         except FileNotFoundError:
             print("Error: Directory not found.")
+            input("[Enter] - Continue")
         except NotADirectoryError:
             print("Error: Not a directory.")
+            input("[Enter] - Continue")
     elif cmd == "help":
         print("Type any command you would normally type in your console/shell.")
-        print("Type info for program info.")
+        print("exit - exit Parashell")
+        print("info - show Parashell info")
+        print("help - show this help")
+        print("next - next page of dir listing")
+        print("prev - previous page of dir listing")
+        input("[Enter] - Continue")
     elif cmd == "show w":
         print("Refer to the GNU GPL, section 15 <https://www.gnu.org/licenses/>.")
     elif cmd == "show c":
@@ -77,11 +148,24 @@ while True:
         info()
     elif cmd == "exit":
         break
+    elif cmd == "next":
+        if page_idx == len(pages)-2:
+            print("No more pages to display")
+            input("[Enter] - Continue")
+        else:
+            page_idx += 1
+    elif cmd == "prev":
+        if page_idx == 0:
+            print("No more pages to display")
+            input("[Enter] - Continue")
+        else:
+            page_idx -= 1
     else:
         exit_code = os.system(cmd)
         if exit_code != 0:
             print(f"Fail (exit code {exit_code})")
         else:
             print(f"Success (exit code {exit_code})")
+        input("[Enter] - Continue")
 
 print("Goodbye")
